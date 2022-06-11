2d0f65d3e8c10261e15c9907bae8c83e47a85ac9
ROUNAK BHARDWAJ <rounakkumar1034@gmail.com>
2022-06-11 17:22:57 +0530

    All right in this stage, add avg reading speed section and some updates that's all done

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import logo from "../img/github-logo.png"
-import logo from "../img/bluegithub.png"
+import logo from "../img/whitegithub.png"
 import "../App.css"
 
 
@@ -8,11 +8,11 @@ export default function Heading() {
     return (
         <div id="main">
             <div className="logo">
-                <div className="t">T</div>
-                <div className="rs">extutils <span className="vLine">|</span></div>
+                <div className="t">&#x0164;</div>
+                <div className="rs">ext&#x01B2;tils <span className="vLine">|</span></div>
             </div>
 
-           <a href="https://www.google.com"> <img id="cat" src={logo} alt="" /> </a> 
+           <a href="https://github.com/BhardwajRohan"> <img id="cat" src={logo} alt="" /> </a> 
             
         </div>
     )
--- src/components/textbox.js
+++ src/components/textbox.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../form.css';
 import LtU from "../img/ltu.png"
 import UtL from "../img/UtL.png"
+
 
 export default function Textbox() {
 
@@ -14,19 +15,27 @@ export default function Textbox() {
         console.log("On Change fire");
         setText(event.target.value);
     }
-    const upperToLower = () =>{
+    const upperToLower = () => {
         let newText2 = text.toLowerCase();
         setText(newText2)
     }
 
-    const [text, setText] = useState(" ")
+    const [text, setText] = useState("")
 
     return (
+
+
         <>
             <div className="fmain">
+                <h3 className='heading'> &#x0174;rite &amp; &#xc2;nalysis 	&#9997;</h3>
                 <textarea value={text} onChange={checkOnChange} autoFocus name="" id="" cols="100" rows="12"></textarea>
+                <p className='Info Info-1'>1. Numbers of Character (Including Spaces ) &rarr; &#9889;<b>{text.length}</b> .</p>
+                <p className='Info'>2. Numbers of Character &rarr; &#9889;<b>{text.length - (text.split(" ").length - 1)}</b> .</p>
+                <p className='Info'>3. Numbers of Word (Including Spaces ) &rarr; &#9889;<b>{text.split(" ").length - 1}</b> .</p>
+                <p className='Info Info-4'>4. Numbers of Sentences &rarr; &#9889;<b>{text.split(". ").length - 1}</b> .</p>
+                <p className='Info Info-4'>5. Avg Reading Speed &rarr; &#9889;<b> {(0.008 * text.split(" ").length).toFixed(2) } min</b>.</p>
             </div>
-            <button onClick={lowerToUpper} className='Primary-btn'><img src={LtU} alt="" /></button>
+            <button onClick={lowerToUpper} className='Primary-btn btn-1'><img src={LtU} alt="" /></button>
             <button onClick={upperToLower} className='Primary-btn'><img src={UtL} alt="" /></button>
 
         </>
