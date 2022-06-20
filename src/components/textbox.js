1f07c50b9557c010a80b3154d6e5e7d3875957ad
ROUNAK BHARDWAJ <rounakkumar1034@gmail.com>
2022-06-20 17:24:43 +0530

    In this commit, added some advance feature in textutils app done...!

--- src/components/textbox.js
+++ src/components/textbox.js
@@ -4,6 +4,9 @@ import LtU from "../img/ltu.png"
 import UtL from "../img/UtL.png"
 import pvpng from "../img/previewicon.png"
 import Print from "../img/print.png"
+import Copy from "../img/copy.png"
+import Remove from "../img/remove.png"
+
 
 
 
@@ -32,14 +35,24 @@ export default function Textbox() {
         setText("");
     }
 
-  
+    const copyText = () => {
+        var Text = document.getElementById("contantBox");
+        Text.select();
+        navigator.clipboard.writeText(Text.value);
+    }
+
+
+    const RemoveExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
 
     return (
 
         <>
             <div className="fmain">
                 <div className="tophead">
-                <h3 className='heading'> &#x0174;rite &amp; &#xc2;nalysis 	&#9997; </h3>
+                    <h3 className='heading'> &#x0174;rite &amp; &#xc2;nalysis 	&#9997; </h3>
                 </div>
                 <textarea value={text} onChange={checkOnChange} autoFocus name="" id="contantBox" cols="100" rows="12"></textarea>
                 <p className='Info Info-1'>1. Numbers of Character (Including Spaces ) &rarr; &#9889;<b>{text.length}</b> .</p>
@@ -61,44 +74,49 @@ export default function Textbox() {
                 </div>
             </div>
 
-        <div className='btn-group'>
-            <button onClick={lowerToUpper} className='Primary-btn btn-1'><img src={LtU} alt="" /></button>
+            <div className='btn-group'>
+                <button onClick={lowerToUpper} className='Primary-btn btn-1'><img src={LtU} alt="" /></button>
 
-            <button onClick={upperToLower} className='Primary-btn'><img src={UtL} alt="" /></button>
-
-
-            <button onClick={() => {
-               const prmptValue = window.confirm("Have you Finished The work ?")
-
-               if(prmptValue === true){
-                console.log(prmptValue); 
-                const txtContant = text;
-                const bodyPrint = document.body.innerHTML;
-                 document.body.innerHTML = txtContant;
-                 window.print();
-                 document.body.innerHTML = bodyPrint;
-               }
-               
-
-            }} className='Primary-btn'><img src={Print} alt="" /></button>
-          
+                <button onClick={upperToLower} className='Primary-btn'><img src={UtL} alt="" /></button>
 
 
+                <button onClick={() => {
+                    const prmptValue = window.confirm("Have you Finished The work ?")
 
-            <button className='Primary-btn btn-spl' onClick={() => {
-                if(text !== ""){
-                    const ModelBox = document.getElementById('modelBox')
-                    ModelBox.classList.add('show');
-                }
-                else{
-                    alert("Please Write some contant then contant Previwe will be displayed !")
-                }
-            }}><img src={pvpng} alt="" /> <span id='Previews'>Preview</span></button>
+                    if (prmptValue === true) {
+                        console.log(prmptValue);
+                        const txtContant = text;
+                        const bodyPrint = document.body.innerHTML;
+                        document.body.innerHTML = txtContant;
+                        window.print();
+                        document.body.innerHTML = bodyPrint;
+                    }
 
-            <button onClick={clear} className='Primary-btn btn-txt'><i className="fa-solid fa-eraser"></i>Clear</button>
 
-            </div> 
-  
+                }} className='Primary-btn'><img src={Print} alt="" /></button>
+
+
+
+
+                <button className='Primary-btn btn-spl' onClick={() => {
+                    if (text !== "") {
+                        const ModelBox = document.getElementById('modelBox')
+                        ModelBox.classList.add('show');
+                    }
+                    else {
+                        alert("Please Write some contant then contant Previwe will be displayed !")
+                    }
+                }}><img src={pvpng} alt="" /> <span id='Previews'>Preview</span></button>
+
+
+                <button onClick={RemoveExtraSpaces} className='Primary-btn btn-txt'> <img src={Remove} alt="" /> RemoveExtraSpaces</button>
+                
+                <button onClick={copyText} className='Primary-btn btn-txt'> <img src={Copy} alt="" /> Copy</button>
+
+                <button onClick={clear} className='Primary-btn btn-txt'><i className="fa-solid fa-eraser"></i>Clear</button>
+
+            </div>
+
         </>
     )
 
